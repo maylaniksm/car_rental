bab5e3414f5a6186ba31c463b12e73e5a7eaf5d1
maylaniksm <63834571+maylaniksm@users.noreply.github.com>
2024-10-06 00:29:14 +0700

    Delete lib directory

--- lib/app/modules/result/bindings/result_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/result_controller.dart';
-
-class ResultBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<ResultController>(() => ResultController());
-  }
-}
